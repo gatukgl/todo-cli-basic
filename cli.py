3917fee01ffd24920b5cde93827e7292ff479b48
Gatuk Chattanon <gatukgl@gmail.com>
2021-08-06 21:29:43 +0700

    Support list, add and exit command for to do list

--- cli.py
+++ cli.py
@@ -1,28 +1,48 @@
 from PyInquirer import prompt
 
 
-def get_todos():
-    return ['hey', 'hello']
+todos = ['Learn python', 'Learn JavaScript']
 
 
-def show_todos(todos):
+def show_todos():
     print('All my todos 👉')
     for item in todos:
         print(f'🔮 {item}')
 
 
+def add_new_todo(task):
+    todos.append(task)
+
+
+def show_add_todo_prompt():
+    question = {
+        'type': 'input',
+        'name': 'task',
+        'message': 'What is your new task?',
+    }
+    answer = prompt(question)
+    add_new_todo(answer['task'])
+
+
 def main():
-    print("My todos list.")
-    questions = {
+    question = {
         'type': 'input',
         'name': 'command',
-        'message': 'What you want to do next >>',
+        'message': 'What you want to do next (list/add/exit) >>',
     }
-    answers = prompt(questions)
-    if answers['command'] == 'list':
-        todos = get_todos()
-        show_todos(todos)
+    answer = prompt(question)
+    if answer['command'] == 'list':
+        show_todos()
+    elif answer['command'] == 'add':
+        show_add_todo_prompt()
+        show_todos()
+    elif answer['command'] == 'exit':
+        exit()
+    else:
+        print('❗️ You entered unavailable task, please enter again!')
+    main()
 
 
 if __name__ == "__main__":
+    print("My todos list.")
     main()
