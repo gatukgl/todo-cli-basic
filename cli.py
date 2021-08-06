005c9bbff8db4456e5801b87aa29803ff852cde9
Gatuk Chattanon <gatukgl@gmail.com>
2021-08-06 20:49:31 +0700

    Add code to list all todos

---
+++ cli.py
@@ -0,0 +1,29 @@
+import click
+from PyInquirer import prompt
+
+
+def get_todos():
+    return ['hey', 'hello']
+
+def show_todos(todos):
+    print('All my todos 👉')
+    for item in todos:
+        print(f'🔮 {item}')
+
+
+@click.command()
+def main():
+    click.echo("My todos list.")
+    questions = {
+        'type': 'input',
+        'name': 'command',
+        'message': 'What you want to do next >>',
+    }
+    answers = prompt(questions)
+    if answers['command'] == 'list':
+        todos = get_todos()
+        show_todos(todos)
+
+
+if __name__ == "__main__":
+    main()
